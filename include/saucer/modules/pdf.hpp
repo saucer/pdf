4072cca1381214d30e05e5c8d1216a3a2c332e34
Curve <fynnbwdt@gmail.com>
2025-01-04 17:28:20 +0100

    feat: implement webkit

--- include/saucer/modules/pdf.hpp
+++ include/saucer/modules/pdf.hpp
@@ -3,11 +3,27 @@
 #include <saucer/webview.hpp>
 
 #include <utility>
+#include <cstdint>
 #include <filesystem>
 
 namespace saucer::modules
 {
     namespace fs = std::filesystem;
+
+    enum class layout : std::uint8_t
+    {
+        portrait,
+        landscape,
+    };
+
+    struct print_settings
+    {
+        fs::path file;
+
+      public:
+        layout orientation{layout::portrait};
+        std::pair<double, double> size{8.3, 11.7};
+    };
 
     class pdf
     {
@@ -17,6 +33,6 @@ namespace saucer::modules
         pdf(saucer::webview *parent);
 
       public:
-        void save(const fs::path &file, std::pair<double, double> size = {8.3, 11.7});
+        void save(const print_settings &settings);
     };
 } // namespace saucer::modules
