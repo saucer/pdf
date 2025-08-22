60bc6c81ec45ae61956357db7cbfb587ef3c753a
Curve <fynnbwdt@gmail.com>
2025-08-22 19:45:51 +0200

    fix(win32): v7 compatibility [skip ci]

--- src/wv2.pdf.cpp
+++ src/wv2.pdf.cpp
@@ -1,24 +1,16 @@
-#include "pdf.hpp"
+#include "pdf.impl.hpp"
 
-#include "win32.app.impl.hpp"
-#include "wv2.webview.impl.hpp"
-
-#include <atomic>
+#include <saucer/win32.app.impl.hpp>
+#include <saucer/wv2.webview.impl.hpp>
 
 namespace saucer::modules
 {
-    void pdf::save(const print_settings &settings)
+    void pdf::impl::save(const settings &settings) // NOLINT(*-function-const)
     {
-        if (!m_parent->parent().thread_safe())
-        {
-            return m_parent->parent().dispatch([this, settings] { return save(settings); });
-        }
+        auto *const web_view = webview->native<false>()->platform->web_view.Get();
+        auto environment     = ComPtr<ICoreWebView2Environment6>{};
 
-        auto &webview = m_parent->native<false>()->web_view;
-
-        ComPtr<ICoreWebView2Environment6> environment;
-
-        if (ComPtr<ICoreWebView2Environment> env; !SUCCEEDED(webview->get_Environment(&env)) || !SUCCEEDED(env.As(&environment)))
+        if (ComPtr<ICoreWebView2Environment> env; !SUCCEEDED(web_view->get_Environment(&env)) || !SUCCEEDED(env.As(&environment)))
         {
             return;
         }
@@ -40,27 +32,26 @@ namespace saucer::modules
 
         std::error_code ec{};
 
-        if (auto parent = settings.file.parent_path(); !fs::exists(parent))
+        if (auto parent = settings.file.parent_path(); !fs::exists(parent, ec))
         {
             fs::create_directories(parent, ec);
         }
 
-        std::atomic_bool finished{false};
-
+        auto finished          = false;
         auto complete_callback = [&](HRESULT, BOOL)
         {
-            finished.store(true);
+            finished = true;
             return S_OK;
         };
 
         auto path     = fs::weakly_canonical(settings.file, ec);
         auto callback = Microsoft::WRL::Callback<ICoreWebView2PrintToPdfCompletedHandler>(complete_callback);
 
-        webview->PrintToPdf(path.wstring().c_str(), print_settings.Get(), callback.Get());
+        web_view->PrintToPdf(path.wstring().c_str(), print_settings.Get(), callback.Get());
 
         while (!finished)
         {
-            m_parent->parent().native<false>()->iteration();
+            parent->native<false>()->platform->iteration();
         }
     }
 } // namespace saucer::modules
